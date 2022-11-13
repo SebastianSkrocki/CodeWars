e719cbb0327850906ff7080c8cebbd2633431c16
SSkrocki <seb.skrocki@gmail.com>
2022-11-13 21:28:25 +0100

    Sum of digits (done)

--- src/SumOfDigits.java
+++ src/SumOfDigits.java
@@ -3,14 +3,14 @@
 public class SumOfDigits {
 
     public static void main (String[] args) {
-        System.out.println(digital_root(3425));
+        System.out.println(digital_root(745409));
     }
     public static int digital_root(int n) {
         int sum_of_digits = 0;
         do{
             sum_of_digits += n % 10;
             n = n / 10;
-        }while (n % 10 != 0);
+        }while (n % 10 != 0 || n > 10);
         n = sum_of_digits;
         if(n < 10){
             return n;
